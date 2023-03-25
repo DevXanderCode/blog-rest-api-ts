cee76c805e22b090d3e4aebfc7f5f459563424de
Alex Chinedu <nwekealexchinedu@gmail.com>
2023-03-25 16:31:01 +0100

    added and set up  multer to handle file upload

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,37 @@
-import path, { dirname } from 'path';
+import path from 'path';
 
 import express, { Express, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import multer from 'multer';
 import { feedRoutes } from './routes';
 import { HttpError } from './types';
 
 const MONGODB_URI = 'mongodb://localhost:27017/messages';
 const __dirname = path.resolve();
-console.log('dirname', __dirname);
 
 const app: Express = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req: Request, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req: Request, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req: Request, file: Express.Multer.File, cb: (arg0: null, arg1: boolean) => void) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-url-encoded
 app.use(bodyParser.json());
+app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'src', 'images')));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
